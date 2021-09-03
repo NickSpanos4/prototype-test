6864fc14269c6925fa6df28914a378ad6952b128
NickSpanos4 <n.spanos4@gmail.com>
2021-09-02 17:03:44 -0700

    First Room Complete
    - Added Keypad
    - Added text over keypad
    - Added interior placement
    
    code involves the corresponding book colour

---
+++ Test File/Assets/Scripts/KeypadController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeypadController : MonoBehaviour
+{
+
+    PassLock pass;
+
+    int rayRange = 100;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            CheckButtonHit();
+        }
+    }
+
+    void CheckButtonHit()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, rayRange))
+        {
+            pass = hit.transform.gameObject.GetComponentInParent<PassLock>();
+
+            if (pass != null)
+            {
+                string num = hit.transform.name;
+                pass.SetNumber(num);
+            }
+        }
+    }
+}
---
+++ Test File/Assets/Scripts/PassLock.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PassLock : MonoBehaviour
+{
+
+    int passPlace;
+    int passLength;
+
+    public string pass = "";
+    public string attemptedPass;
+
+    public Transform openDoor;
+    public GameObject textDisplay;
+
+
+    private void Start()
+    {
+        passLength = pass.Length;
+    }
+   
+    void CheckPass()
+    {
+        if (attemptedPass == pass)
+        {
+            StartCoroutine(Open());
+        }
+
+        else
+        {
+            Debug.Log("Incorrect Passcode");
+        }
+    }
+
+    IEnumerator Open()
+    {
+        openDoor.Rotate(new Vector3(0, -90, 0), Space.World);
+
+        yield return new WaitForSeconds(4);
+
+        openDoor.Rotate(new Vector3(0, 90, 0), Space.World);
+    }
+
+    public void SetNumber(string num)
+    {
+        passPlace++;
+
+        if (passPlace <= passLength)
+        {
+            attemptedPass += num;
+            textDisplay.GetComponent<Text>().text = attemptedPass;
+        }
+
+        if (passPlace == passLength)
+        {
+            CheckPass();
+
+            attemptedPass = "";
+            passPlace = 0;
+        }
+    }
+}
